dccee58a026f7933d809006b22448646393a1122
KaterynaTrofimyuk <73081823+Katrin18@users.noreply.github.com>
2020-12-05 00:55:29 +0200

    dz-2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, {Component} from 'react';
-import {users} from "./database/Users";
-import UserComponent from "./UserComponent";
-import './User.css';
 
 class App extends Component {
-  render() {
-    return (
-        <div>
-          {
-            users.map((user, index) => (<UserComponent item={user} key={index}/>))
-          }
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
 }
 
-export default App
+export default App;
