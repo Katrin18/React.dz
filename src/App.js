731432d60ee0fbfdce0ea21c8a8eecb0946e7e42
KaterynaTrofimyuk <73081823+Katrin18@users.noreply.github.com>
2020-12-05 03:34:03 +0200

    dz-2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import AllPosts from "./AllPosts";
+
 
 class App extends Component {
     render() {
         return (
             <div>
-                
+                <AllPosts/>
             </div>
         );
     }
