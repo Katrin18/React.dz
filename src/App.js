d0687d144a1a10edb43674f21133876daeded582
KaterynaTrofimyuk <73081823+Katrin18@users.noreply.github.com>
2020-12-09 00:36:06 +0200

    hw-3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, {Component} from 'react';
-import AllPosts from "./AllPosts";
-
 
 class App extends Component {
     render() {
         return (
             <div>
-                <AllPosts/>
+                
             </div>
         );
     }
