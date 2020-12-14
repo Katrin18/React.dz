1d5a0e299e8ef315bbcd55e8dd4d2eccf8b42138
KaterynaTrofimyuk <73081823+Katrin18@users.noreply.github.com>
2020-12-14 02:47:15 +0200

    hw-5

--- src/CommentServer.js
+++
@@ -1,7 +0,0 @@
-export default class CommentServer{
-    url="https://jsonplaceholder.typicode.com/comments";
-
-    async comments(){
-        return await fetch(this.url).then(value=>value.json());
-    }
-};
--- src/PostsServer.js
+++
@@ -1,7 +0,0 @@
-export default class PostsServer{
-    url="https://jsonplaceholder.typicode.com/posts";
-
-   async posts(){
-        return await fetch(this.url).then(value=>value.json());
-}
-};
